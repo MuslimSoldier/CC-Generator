1513e8eadd2dcd1998b9fc5dba9005b35308d839
Abdulmunim Jundurahman <promunim@gmail.com>
2018-10-21 15:13:07 -0400

    The Main Index File

---
+++ index.php
@@ -0,0 +1,285 @@
+<!DOCTYPE html>
+<html lang="en"> 
+<head> <meta charset="utf-8"> 
+<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+<meta name="description" content=""> <meta name="author" content=""> 
+<link rel="shortcut icon" href="http://www.sayapro.us/images/icon.ico" type="image/ico"> 
+
+<link href="http://www.sayapro.us/themes/default/css/style.default.css" rel="stylesheet"> 
+
+<style>
+.spinner {
+  width: 40px;
+  height: 40px;
+
+  position: relative;
+  margin: 100px auto;
+}
+
+.double-bounce1, .double-bounce2 {
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+  background-color: #333;
+  opacity: 0.6;
+  position: absolute;
+  top: 0;
+  left: 0;
+  
+  -webkit-animation: sk-bounce 2.0s infinite ease-in-out;
+  animation: sk-bounce 2.0s infinite ease-in-out;
+}
+</style>
+
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/font-awesome.min.css">
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script> 
+<script src="http://www.sayapro.us/themes/default/js/jquery-migrate-1.2.1.min.js"></script> 
+<script src="http://www.sayapro.us/themes/default/js/bootstrap.min.js"></script> 
+<script src="http://www.sayapro.us/themes/default/js/jquery.sparkline.min.js"></script> 
+<script src="http://www.sayapro.us/themes/default/js/toggles.min.js"></script> 
+<script src="http://www.sayapro.us/themes/default/js/retina.min.js"></script> 
+<script src="http://www.sayapro.us/themes/default/js/jquery.cookies.js"></script> 
+<script src="http://www.sayapro.us/themes/default/js/custom2.js"></script> 
+<script src="http://www.sayapro.us/themes/default/js/checker.js" type="text/javascript"></script> 
+<script src="http://www.sayapro.us/themes/default/js/jquery.datatables.min.js"></script> 
+<script src="http://www.sayapro.us/themes/default/js/chosen.jquery.min.js"></script> 
+<!-- addons --> 
+<link href="//fonts.googleapis.com/css?family=Orbitron" rel="stylesheet">
+<link href="//db.onlinewebfonts.com/c/3116cf7d5d85070c2c0d4b96bfee7d8d?family=GeosansLight" rel="stylesheet" type="text/css"/>
+<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script> 
+<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script> 
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.6.4/sweetalert2.min.css"> 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.6.4/sweetalert2.min.js"></script> 
+<script src="http://www.sayapro.us/js/sayapro/refreshinfo.js"></script> 
+<script src="http://www.sayapro.us/js/sayapro/quick_access.js"></script> 
+<script type="text/javascript"> 
+var fraudstercrew_URL = 'http://www.fraudstercrew.in';
+ 
+var ROOT = 'http://www.sayapro.us';
+ 
+var SPECIALACCESS = 0;
+ 
+</script>
+
+<script>
+function copy() {
+  /* Get the text field */
+  var copyText = document.getElementById("output");
+
+  /* Select the text field */
+  copyText.select();
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  /* Alert the copied text */
+  alert("All CCs have been copied successfully! ");
+} 
+</script>
+
+<body class=""> 
+<header>
+<br/>
+<section class="title"><img src="http://ayyaantuu.net/wp-content/uploads/2016/08/abbaa_gadaa.jpg" alt="Icon"> Adda Bilisummaa Oromoo! [ABO Coders] | Carders Tools </section>
+<div class="hh-title"> Carders Tools! <br />
+<i class="fa fa-cc-mastercard" aria-hidden="true"></i> 
+<i class="fa fa-cc-discover" aria-hidden="true"></i> 
+<i class="fa fa-cc-visa" aria-hidden="true"></i> 
+<i class="fa fa-cc-jcb" aria-hidden="true"></i>
+<i class="fa fa-cc-amex" aria-hidden="true"></i> 
+<i class="fa fa-cc-diners-club" aria-hidden="true"></i></div>
+</header>
+<center>
+<span style="font-size:25px;"><img src="http://oromoliberationfront.org/english/wp-content/uploads/2017/02/ALAABAANew1.gif"></img></span>
+<span style="font-size:20px;"><img src="https://lh3.googleusercontent.com/-YX8jm0V1Elw/VhEyW5xUB5I/AAAAAAAAjYA/iYk3gCDw_MA/s1600/ethiopia-flag-pole-animated.gif"></img></span>
+<span style="font-size:25px;"><img src="http://oromoliberationfront.org/english/wp-content/uploads/2017/02/ALAABAANew1.gif"></img></span></br>
+</center>
+
+
+<!-- headerbar -->
+<script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+<script src="http://www.sayapro.us/v2/public/js/power.js"></script>
+<script type="text/javascript"> 
+function fapfap(card, mxcards, loop){ var ju = Math.floor(Math.random()*(mxcards+1-2))+2;
+ 
+for(var i=loop;
+i>=1;
+i--){ var cdi=sbtStringSpRnd(card,"x","0123456789");
+ 
+var cf=sbtString(cdi," -/abcdefghijklmnopqrstuvwyzABCDEFGHIJLMNOPQRSTUVWYZ");
+ 
+var clcd=chkLCD(cf);
+ var ccck=chkCCCksum(cf,ju);
+ if(clcd && ccck)break;
+ } return cdi;
+ } 
+function gencard(card,jumlah,delim) { var x ='';
+ 
+for (var i = jumlah - 1;
+ i >= 0;
+ i--) { x +=fapfap(card,2000,5000) + delim;
+ } return x;
+ } 
+function rndccv(cdif){ for (var i = 10 - 1;
+ i >= 0;
+ i--) { var binC=cdif.toString();
+ 
+var tipo = parseInt(binC.substring(0,1));
+ if(tipo==3) var eccv=(Math.floor(Math.random() * (9998 - 1102 + 1)) + 1102);
+ 
+else var eccv=(Math.floor(Math.random() * (998 - 112 + 1)) + 112);
+ if(i > 9) break;
+ } return eccv;
+ } function rndexp(a) { var dnowi=new Date();
+ var mes=unilenS(rnd(1,12),2,'0',0);
+
+ var year=(dnowi.getFullYear()+rnd(2,5));
+ if(a == 'm') return mes;
+ if(a == 'y') return year;
+ } function acom(cc){ cc = sbtString(cc," ");
+ var count = 0;
+ var length = 16;
+ if(leftS(cc,1) == 3) length -=1;
+ if(cc.length < length){ for (var i = length - 1;
+ i >= 0;
+ i--) { if(cc.length < length) cc += 'x';
+count++;
+ } } return cc;
+ } function _fap(s,k){ var f = $('#ccbin');
+ if(true != s.is(':checked')) return f.val(f.val().replace('|'+k, ''));
+ if(s.is(':checked')) return f.val(f.val() + '|' + k);
+ } function bin2cc(cc,a,ou){ b = cc.split('|');
+ var _bin2cc = function(a,b) { var _on = '';
+ for (var x = 0;
+ x < b.length;
+ x++) { if(x == 0) b[x] = '';
+ if(b[x] == '(m)') _on += rndexp('m');
+ if(b[x] == '(y)') _on += rndexp('y');
+ if(b[x] == '(cvv)') _on += rndccv(a);
+ _on +=b[x];
+ if(x < b.length-1) _on += '|';
+ } var _on = sbtString(_on," (-/abcdefghijklmnopqrstuvwxyzABCDEFGHIJLMNOPQRSTUVWXYZ)");
+ return _on;
+ };
+ var _cl = function(v,x){ if(x == 'Global Carding Inc.') return v.join("\n");
+ if(x == 'json'){ var _f = '';
+ for (var i = 0;
+ i < v.length;
+ i++) { _f += '{';
+ var u = v[i].split('|');
+ for (var k = 0;
+ k < u.length;
+ k++) { if(k == 0) _f+= '"number":"'+u[k]+'"';
+ if(k == 1) _f+= '"month":"'+u[k]+'"';
+ if(k == 2) _f+= '"year":"'+u[k]+'"';
+ if(k == 3) _f+= '"cvv":"'+u[k]+'"';
+ if(k < u.length-1) _f +=',';
+ } _f += '}';
+ if(i < v.length-1) _f +=',';
+ } return JSON.stringify(JSON.parse('[' + _f + ']'),null,3);
+ } return v.join("\n");
+ };
+ if(b){cc=b[0]};
+ if(a > 50000) a = 50000;
+ if(cc == '' || cc == null || cc == undefined) return alert('Bin empty!');
+ if(a == '' || a == null || a == undefined) return alert('Amounts empty!');
+ ptrn = '';
+ cc = acom(cc);
+ for (var i = 0;
+ i < b.length;
+ i++) { if(i == 0) b[i] = cc;
+ ptrn += sbtString(b[i], " ");
+ if(i < b.length-1) ptrn +='|';
+ } $('#ccbin').val(ptrn);
+ cc = sbtString(cc," -/abcdefghijklmnopqrstuvwyzABCDEFGHIJLMNOPQRSTUVWYZ");
+ ox = gencard(cc,1,"\n");
+ op = [];
+ if(ox != cc+"\n"){ for (var i = a - 1;
+ i >= 0;
+ i--) { op.push(gencard(cc,1,"") + _bin2cc(ox,b));
+ } }else var op = ['You enter full card number? try see Pro Tips'];
+ _cl(op,$('#_cl').val());
+ ou.val(_cl(op,$('#_cl').val()));
+ }</script><title>CC Generator - Powered by Global Carding Inc. </title>
+ <script type="text/javascript"></script><div class="contentpanel"> 
+ <!--contentpanel--> 
+ <div class="panel panel-default">
+ <div class="panel-heading"> 
+ <h3 class="Lbutton" align="right">
+   <center>----------| ABO CC Gen V1.0 |----------</center> </h3> </div>
+  <div class="panel-body"> 
+  <div class=""> 
+  <div class="Ilabel"> 
+  <i class="fa fa-gg"></i> Enter your BIN&ensp;&ensp; 
+  <input class="input_text" name="ccbin" id="ccbin" placeholder="434256 or 400314xxxxxxxxxx" required="" type="text"> 
+  </br>
+  <strong>
+  <label style="padding-left:9em">
+  <input style="padding-left:3em" onchange="_fap($(this), '(m)')" autocomplete="off" type="checkbox"></input><font color="#7EC7C0"> Month </font></label> <label>
+  <input style="padding-left:9em" onchange="_fap($(this), '(y)')" type="checkbox" autocomplete="off"></input><font color="#7EC7C0">   Year </font> </label> <label>
+  <input style="padding-left:9em" onchange="_fap($(this), '(cvv)')" type="checkbox" autocomplete="off"></input><font color="#7EC7C0"> CVV </font></label> 
+  </strong>
+  </div> 
+  <div class="Ilabel"> <i class="fa fa-shopping-bag" aria-hidden="true">Amounts : </i> 
+  <input onclick="this.select()" class="input_text" name="cccount" id="cccount" placeholder="50" required="" value="50" type="number"> </div> 
+  
+  <div class="Ilabel"> <label color="#7EC7C0" class="fa fa-cogs" aria-hidden="true"> <font color="#7EC7C0"> Format : </font></label> 
+  <select id="_cl" class="input_text"> 
+  <option value="Global Carding Inc.">Global Carding Inc.</option> 
+  <option value="json">JSON</option> 
+  <option value="sql" disabled="">SQL</option> </select> </div> 
+  <div class="form-group col-sm-15"> </div> <div class="form-group col-sm-15"> 
+
+
+
+
+  
+ <center style="padding-top:15px"> 
+ <a class="Lbutton"  onclick="copy()"><i class="fa fa-bolt"></i> COPY ALL CC</a>
+ <a onclick="bin2cc($('#ccbin').val(),$('#cccount').val(),$('#output'))" type="submit" class="Lbutton"><i class="fa fa-bolt"aria-hidden="true"></i>&nbsp;GENERATE CC</a> </center>
+  <textarea id="output" class="textarea" cols="25" rows="15" readonly="" placeholder=""></textarea>
+   </div> </div>  </div>
+
+
+   
+  <div id="result"> </div> </div> </div> </div> <!--end content--></div><script> $(document).ready(function(){ if(window.console || "console" in window) { console.log("%cSAYA%cPRO%c.US", "color:#a94442;
+ font-size:40px;
+", "color:#1CAF9A;
+ font-size:40px;
+", "color:#636E7B;
+ font-size:40px;
+");
+ console.log("%c Hey! we need someone like you. Maybe we can talk ? ", "color:#003087;
+ font-size:12px;
+");
+ console.log("%c Contact : spammersking@hi2.in", "color:#003087;
+ font-size:12px;
+");
+ } });
+ function closeAlert(alert_class){ var findIndexInData = function(data, property, value) { var result = -1;
+ data.some(function (item, i) { if (item[property] === value) { result = i;
+ return true;
+ } });
+ return result;
+ };
+ var closed = $.cookie("clAlert");
+ if(closed == null) var closed = JSON.stringify([]);
+ var closed1 = JSON.parse(closed);
+ if(findIndexInData(closed1, 'name', alert_class) == -1) closed1.unshift({'name': alert_class});
+ $.cookie("clAlert", JSON.stringify(closed1), { path: '/', expires: 365 });
+ } function closedAlert_READY(){ $(JSON.parse($.cookie("clAlert"))).each(function(i,el){ if(el.name != 'undefined'){ $('.'+el.name).css('display','none');
+ } });
+ }</script>
+
+ <span id="out">
+<span/><footer><i class="fa fa-credit-card-alt" aria-hidden="true"></i>
+<strong>ABO CC GEN V1.0 </strong><i class="fa fa-credit-card-alt" aria-hidden="true"></i> </br>
+<i class="fa fa-user-secret" aria-hidden="true"></i> Developers <a href="https://telegram.me/ABOCODERS" style="color:#00ffff;">ABO Coders</a><br />
+<i class="fa fa-user-secret" aria-hidden="true"></i><a href="https://telegram.me/HQCarder" style="color:#00ffff;">Carders King</a><br /> 
+<a target="_BLANK" href="https://www.t.me/gbcarding" style="color:#00ffff;">Powered by <strong> GLOBAL CARDING</strong> </a><i class="fa fa-certificate" aria-hidden="true"></i><br />
+
+ </body></html>
